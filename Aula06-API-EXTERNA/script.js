944b61fc715d122c52c1b60a5138257e536e6774
Giovanna <giovanna.aalves12@gmail.com>
2026-03-09 16:05:35 -0300

    Adicionando novos arquivos e atualizando a função X

--- Aula06-API-EXTERNA/script.js
+++ Aula06-API-EXTERNA/script.js
@@ -1,43 +1,93 @@
 /*
-CONSMINDO UMA API COM JAVASCRIPT
+CONSUMINDO UMA API COM JAVASCRIPT
 API: https://dog.ceo/api
 */
+//================================
+// 1- SELECIONAR ELEMENTOS DO HTML
+//================================
 
-// 1- SELECIONAR ELEMNTOS DO HTML
-
-// Pegar a imagem do cachorro
+//Pegar a imagem do cachorro
 const dogImage = document.getElementById("docImage");
-// Elemento onde aparece o nome da raça
-const breedName =document.getElementById("breedName");
-// Botão que busca cachorro aleatório
+//elemento onde aparece o nome da raça
+const breedName = document.getElementById("breedName");
+//botão que busca cachorro aleatório
 const randomBtn = document.getElementById("randomBtn");
-// Botão que busca a raça
+//botão que busca a raça
 const searchBtn = document.getElementById("searchBtn");
-// Campo de input onde o usuário digita a raça
+//campo de input onde o usuário digita a raça
 const breedIpnut = document.getElementById("breedIpnut");
-// 
-const dogArea = document.getElementById(".dog-area");
+// área onde a imagem aparece
+const dogArea = document.querySelector(".dog-area");
 
+//=======================
+// 2- URL BASE DA API
+//=======================
 
-// ========== 2 ==========
-// BASE DA API
-// Endereço principal da API
+//endereço principal da API
 const API_BASE = "https://dog.ceo/api"
 
-// ========== 3 ==========
-// FUNÇÃO QUE CHAMA A API
+//===========================
+// 3- FUNÇÃO QUE CHAMA A API
+//===========================
 
-// função  assíncrona que faz requisição HTTP
-async function fetchFromApi(endpoint) {
+//função assíncrona que faz requisição HTTP
+async function fetchFromApi(endpoint){
 
-    // adiciona classe de loading (mostra "Carregando")
+    //adiciona classe de loading (mostra "Carregando")
     dogArea.classList.add("loading")
 
     try {
-        // montar a url completa da requisição
-        const url = '${API_BASE}${endpoint}';
+        //montar a URL completa da requisição
+        const url = `${API_BASE}${endpoint}`;
 
-        // mostrar no console a URL chamada
-        console.log("Requisição:", url)
+        //mostrar no console a URL Chamada
+        console.log("Requisição:", url);
+
+        //faz requisição HTTP para a API
+        const response = await fetch(url);
+
+        //Converter para JSON
+        const data = await response.json();
+
+        //Mostrar a resposta console
+        console.log("Resposta:", data);
+
+        //Verificar se a API retornou com sucesso
+        if (data.status === "success") {
+            //URL da imagem retornada pela API
+            const imageUrl = data.message;
+            //colocar a imagem no element <img>
+            dogImage.src = imageUrl;
+            //extrai a raça da URL
+            const breed = imageUrl.split("/")[4];
+            //substituir "-" por espaço
+            const formattedBreed = breed.replace(/-/g," ");
+
+            //deixa a primeira letra maiúscula
+            const finalBreed = 
+                formattedBreed.charAt(0).toUpperCase() +
+                formattedBreed.slice(1);
+
+            //mostra o nome da raça na tela
+            breedName.textContent = finalBreed;
+        }
+    }catch (error){
+        //mostrar erro no console
+        console.error("Erro:", error)
+
+        //mensagem de erro para o usuário
+        breedName.textContent = "Erro ao carregar";
+
+        //remove imagem
+        dogImage.src = "";
+    } finally {
+
+        // remove estado de loadind
+        dogArea.classList.remove("loading");
+
     }
+
 }
+fetchFromApi ("/breeds/image/random");
+
+
